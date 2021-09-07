0564bf4b1328f911822957b57be44e1e9888dade
Hugo Pfister <hugo@Hugos-MacBook-Pro.local>
2021-09-07 18:10:41 +0800

    check.py file

---
+++ check.py
@@ -0,0 +1,6 @@
+import matplotlib.pyplot as plt #this is a plotting package
+import numpy as np #this is an array manipulation package
+import yt #this is a post-processing package
+from timeit import timeit #timer to check the bottlenecks of your codes
+import scipy as sci
+print('Ready to follow the course!!')
